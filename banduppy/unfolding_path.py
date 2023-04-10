1f9f8d0eb557c6868ee80cc2ca98cac11722ebe7
nicholascheng94 <33250303+nicholascheng94@users.noreply.github.com>
2023-04-10 14:50:29 +0800

    Compatability with python>=3.10

--- banduppy/unfolding_path.py
+++ banduppy/unfolding_path.py
@@ -10,7 +10,10 @@ from  irrep.bandstructure import BandStructure
 if  irrep.__version__ <"1.6.2" :
     raise ImportError("A critical bug was found in irrep-1.6.1, which caused incorrect results for unfolding with spin-orbit. Please ipdate irrep to 1.6.2 or newer (when available)")
 
-from collections import Iterable
+try:
+    from collections.abc import Iterable
+except ImportError:
+    from collections import Iterable
 
 class Unfolding():
 
